224e1c38ced4c86e1da8407e90ffa77d31a2ae63
Silas Almeida <silas.sena@toodoo.com.br>
2021-04-21 04:52:28 -0300

    feat(babel.config): add babel config

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+        }
+      }
+    ],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'babel-plugin-transform-typescript-metadata'
+  ],
+  ignore: [
+    '**/*.spec.ts' // Ignora arquivos de testes no bundle
+  ]
+}
