03675adfe9a87485d399019818c452b29ac7487a
Silas Almeida <silas.sena@toodoo.com.br>
2021-04-21 20:40:29 -0300

    feat(app): add errors from celebrate

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import './database/connection'
 
 import express from 'express'
 import { routes } from './routes'
+import { errors } from 'celebrate'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 export default app
