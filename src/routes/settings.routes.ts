05fc090be2549050154a264536e957d7248e3e8a
Silas Almeida <silas.sena@toodoo.com.br>
2021-04-21 20:51:58 -0300

    feat(routes): add validation middleware

--- src/routes/settings.routes.ts
+++ src/routes/settings.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import { settingsValidator } from '../validators/settingsValidators'
 import SettingsController from '../controllers/SettingsController'
 
 const router = Router()
 
-router.post('/settings', SettingsController.create)
+router.post('/settings', settingsValidator, SettingsController.create)
 
 export default router
