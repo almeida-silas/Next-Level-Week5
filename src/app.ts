aaa0b95000783848d7a4489be1ca6d03242e27b0
Silas Almeida <silasadssn@gmail.com>
2021-04-23 14:10:51 -0300

    feat(app): add swagger setup

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import './database/connection'
 import express from 'express'
 import { routes } from './routes'
 import { errors } from 'celebrate'
+import { setupSwagger } from './config/swagger'
 
 const app = express()
 
+setupSwagger(app)
 app.use(express.json())
 app.use(routes)
 app.use(errors())
