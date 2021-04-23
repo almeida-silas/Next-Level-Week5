a0f8dfc307d2ef0fda9253db49777e00aef41bdd
Silas Almeida <silasadssn@gmail.com>
2021-04-23 12:42:14 -0300

    feat(global): add swagger

---
+++ src/config/swagger.ts
@@ -0,0 +1,9 @@
+import { Express } from 'express'
+import { serve, setup } from 'swagger-ui-express'
+
+import swaggerConfig from '../../docs'
+import { noCache } from '../middlewares/noCache'
+
+export const setupSwagger = (app: Express): void => {
+  app.use('/api/docs', noCache, serve, setup(swaggerConfig))
+}
