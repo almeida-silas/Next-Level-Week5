068a00fdb8f334e9e4bbe960f8f1e8dffafd71c9
Silas Almeida <silasadssn@gmail.com>
2021-04-24 02:43:35 -0300

    refactor(schemes): make chat optional

--- src/schemes/SettingsSchema.ts
+++ src/schemes/SettingsSchema.ts
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 const SettingsSchema = Joi.object().keys({
   username: Joi.string().required(),
-  chat: Joi.boolean().required().default(false)
+  chat: Joi.boolean().optional().default(false)
 })
 
 export { SettingsSchema }
