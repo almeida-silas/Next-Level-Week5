655f192a209cb2e072da7c4c8c5040662c8c4994
Silas Almeida <silasadssn@gmail.com>
2021-04-24 01:21:57 -0300

    refactor(app): remove celebrate errors and add global handler errors

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,14 @@ import './database/connection'
 
 import express from 'express'
 import { routes } from './routes'
-import { errors } from 'celebrate'
 import { setupSwagger } from './config/swagger'
+import { globalHandler } from './errors/handler'
 
 const app = express()
 
 setupSwagger(app)
 app.use(express.json())
 app.use(routes)
-app.use(errors())
+app.use(globalHandler)
 
 export default app
