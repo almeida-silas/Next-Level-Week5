a212488a6485ca67b6edca86d1b42c9c252e3b70
Silas Almeida <silasadssn@gmail.com>
2021-04-24 06:04:59 -0300

    feat(services): add services

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from 'typeorm'
+
+import { SettingsRepository } from '../repositories/SettingsRepository'
+import { Setting } from '../entities/Setting'
+import AppError from '../errors/AppError'
+
+interface ISettingsCreate {
+  username: string
+  chat: boolean
+}
+
+class SettingsService {
+  async create ({ username, chat }: ISettingsCreate): Promise<Setting| AppError> {
+    const settingsRepository = getCustomRepository(SettingsRepository)
+
+    const userAlreadyExists = await settingsRepository.findOne({ username })
+
+    if (userAlreadyExists) {
+      throw new AppError('User already exists', 400, ['username'])
+    }
+
+    const settings = settingsRepository.create({
+      username,
+      chat
+    })
+
+    await settingsRepository.save(settings)
+
+    return settings
+  }
+}
+
+export default new SettingsService()
