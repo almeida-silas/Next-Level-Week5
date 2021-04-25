971342c74c6b55d296c608c88aa13788be8b9888
Silas Almeida <silasadssn@gmail.com>
2021-04-24 22:56:07 -0300

    feat(message): add flow to the Message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import settings from './settings.routes'
 import users from './users.routes'
+import messages from './messages.routes'
 
 const routes = Router()
 
 routes.use('/api', settings)
 routes.use('/api', users)
+routes.use('/api', messages)
 
 export { routes }
